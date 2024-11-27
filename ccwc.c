a03058528f79ca794288b1771c0754d15c64b5de
Steven Locorotondo <steven.locorotondo@justeattakeaway.com>
2024-11-27 22:48:15 +0000

    add handling when user doesn't use option

--- ccwc.c
+++ ccwc.c
@@ -17,12 +17,28 @@ void count_bytes(FILE *file_ptr, int *count) {
     }
 }
 
+int ends_with_txt(char *file_name) {
+    // find first occurence of '.' and return pointer to this character
+    file_name = strrchr(file_name, '.');
+
+    if (file_name != NULL)
+        // use pointer to '.' to get the rest of the string (i.e. .txt)
+        return strcmp(file_name, ".txt");
+
+    return -1;
+}
+
 int main(int argc, char *argv[]) {
     printf("Hello world!\n");
-    // pointer is a reference to a particular position in the opened file
 
+    int first_arg_is_filename = ends_with_txt(argv[1]);
+
+    // pointer is a reference to a particular position in the opened file
     FILE *file_ptr;
-    file_ptr = fopen(argv[2], "rb");
+    file_ptr = fopen(argv[1], "rb");
+    if (first_arg_is_filename == -1) {
+        file_ptr = fopen(argv[2], "rb");
+    }
 
     // check if the file is opened successfully
     if (file_ptr == NULL) {
@@ -33,7 +49,7 @@ int main(int argc, char *argv[]) {
     int count;
     count = 0;
 
-    if (strcmp(argv[1], "-c") == 0) {
+    if (first_arg_is_filename == 0 || strcmp(argv[1], "-c") == 0) {
         count_bytes(file_ptr, &count);
     } else if (strcmp(argv[0], "xxx") == 0) {
     }
